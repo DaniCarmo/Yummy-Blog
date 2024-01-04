6adc866d4a92d670291abbcfb5be15684b0846bc
DaniCarmo <dani93elle@hotmail.com>
2024-01-04 16:08:59 +0000

    modify settings and url files

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ["SECRET_KEY"] = 'bnvd8**&^sdbsb5£'
+os.environ["DEVELOPMENT"] = "True"
--- main/settings.py
+++ main/settings.py
@@ -9,21 +9,23 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+if os.path.exists('env.py'):
+    import env
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$dyueh7$yvnoby9(-yl4hc1nf&hq2k%&gq&gka31b0ke7hy-p+'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = []
 

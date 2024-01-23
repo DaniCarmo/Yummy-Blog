16710c70c57d5dbd149a8034a4e240fa002df059
DaniCarmo <dani93elle@hotmail.com>
2024-01-23 11:29:14 +0000

    add djrichtextfield config and url

--- env.py
+++ env.py
@@ -1,4 +1,5 @@
 import os
 
 os.environ["SECRET_KEY"] = 'bnvd8**&^sdbsb5£'
-os.environ["DEVELOPMENT"] = 'True'
+os.environ["DEVELOPMENT"] = "True"
+os.environ["CLOUDINARY_URL"] = "cloudinary://575441687615991:z1DVJjxHTsUlL-OcCuaidJ5WQ4s@dsf9qtvyk"

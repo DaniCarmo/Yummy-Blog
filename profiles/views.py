52a320c1f796f6ae28094787ae523268a2ae5f06
DaniCarmo <dani93elle@hotmail.com>
2024-02-12 10:45:38 +0000

    extend base to profile.html and update url path

--- profiles/views.py
+++ profiles/views.py
@@ -1,20 +1,16 @@
 from django.views.generic import TemplateView, UpdateView
-from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 
 from .models import Profile
-from .forms import ProfileForm
 
 
 class Profiles(TemplateView):
     """User Profile View"""
-
     template_name = "profiles/profile.html"
 
     def get_context_data(self, **kwargs):
         profile = Profile.objects.get(user=self.kwargs["pk"])
         context = {
             "profile": profile,
-            'form': ProfileForm(instance=profile)
         }
 
         return context

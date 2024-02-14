bc14bd04d829319a9fc98c0ef02795ca713b5424
DaniCarmo <dani93elle@hotmail.com>
2024-02-14 15:09:34 +0000

    update nav and footer color

--- main/settings.py
+++ main/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['https://danicarmo.github.io/Yummy-Blog-p4/']
+ALLOWED_HOSTS = []
 
 
 # Application definition
